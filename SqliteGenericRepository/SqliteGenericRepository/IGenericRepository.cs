56a8462dd6c9daaab056c2a9c9719a7cd5df0bec
DiKol <dmytrokolesnyk@gmail.com>
2023-02-03 00:24:24 +0200

    Initial commit

---
+++ SqliteGenericRepository/SqliteGenericRepository/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System.Linq.Expressions;
+
+namespace SalesAppApi.Data
+{
+    /// <summary>
+    /// Generic Repository for SQLLite
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenericRepository<T> where T : class, new()
+    {
+        Task<List<T>> Get();
+        Task<T> Get(int id);
+        Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null);
+        Task<T> Get(Expression<Func<T, bool>> predicate);
+        AsyncTableQuery<T> AsQueryable();
+        Task<int> Insert(T entity);
+        Task<int> Update(T entity);
+        Task<int> Delete(T entity);
+    }
+}
